f976607d4a47e90c85e71614a5001a6e99dd179b
pwnhyo <8891ee1234@gmail.com>
2023-12-07 03:52:07 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Vully
 Vulnerabilty Detector using GNNs
+
+### joern install
+```
+wget https://github.com/joernio/joern/releases/latest/download/joern-install.sh
+chmod +x ./joern-install.sh
+sudo ./joern-install.sh
+joern
+```
+
+if you use Mac OS, Do brew install `md5sha1sum` and `coreutils` first.
--- data_handler/data_handler.py
+++ data_handler/data_handler.py
@@ -1,19 +1,26 @@
 import os
 
-data_folder = "../data/CWE121_Stack_Based_Buffer_Overflow"
 
-# s01부터 s09까지의 폴더를 순회합니다.
-for i in range(1, 10):
-    folder_name = f"s{i:02d}"
-    folder_path = os.path.join(data_folder, folder_name)
+# "../data/CWE121_Stack_Based_Buffer_Overflow"
+def get_c_code_from_file(file_path):
+    codes = []
+    data_folder = file_path
 
-    # 폴더 내의 .C 파일을 가져옵니다.
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".c"):
-            file_path = os.path.join(folder_path, file_name)
-            # 파일을 열어서 C 코드를 읽어옵니다.
-            with open(file_path, 'r') as file:
-                c_code = file.read()
-            # 읽어온 C 코드를 출력하거나 원하는 작업을 수행합니다.
-                print(c_code)
+    # s01부터 s09까지의 폴더를 순회합니다.
+    for i in range(1, 10):
+        folder_name = f"s{i:02d}"
+        folder_path = os.path.join(data_folder, folder_name)
+
+        # 폴더 내의 .C 파일을 가져옵니다.
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".c"):
+                file_path = os.path.join(folder_path, file_name)
+                # 파일을 열어서 C 코드를 읽어옵니다.
+                with open(file_path, 'r') as file:
+                    c_code = file.read()
+                # 읽어온 C 코드를 출력하거나 원하는 작업을 수행합니다.
+                codes.append(c_code)
+    return codes
+
+
 
